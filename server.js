74497e936bc3aa96b8cd80b0f6c1e4c5d8ec4b77
san4osq <san4osq@ya.ru>
2015-03-27 22:25:42 +0200

    z

---
+++ server.js
@@ -0,0 +1,7 @@
+var server = BinaryServer({port: 9000});
+server.on('connection', function(client){
+    client.on('stream', function(stream, meta){
+        var file = fs.createWriteStream(meta.file);
+        stream.pipe(file);
+    });
+});
